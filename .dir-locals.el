d97a4c8cdfc6cb3e1e64a727141b945a739580ab
Teemu Likonen <tlikonen@iki.fi>
2020-11-14 14:03:25 +0200

    Lisätään .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((indent-tabs-mode . nil)))
+ (latex-mode . ((mode . auto-fill)
+                (fill-column . 78)
+                (TeX-engine . luatex)
+                (TeX-master . "opas.tex")
+                (TeX-open-quote "''"))))

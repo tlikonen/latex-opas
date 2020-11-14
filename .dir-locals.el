b8727e54d0f41f4b2f7cabdcec370a34678897bc
Teemu Likonen <tlikonen@iki.fi>
2020-11-14 19:07:39 +0200

    .dir-locals-säätöä

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,9 +1,14 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((indent-tabs-mode . nil)))
+((nil . ((fill-column . 79)
+         (indent-tabs-mode . nil)))
  (latex-mode . ((mode . auto-fill)
-                (fill-column . 78)
+                (mode . outline-minor)
                 (TeX-engine . luatex)
                 (TeX-master . "opas.tex")
-                (TeX-open-quote "''"))))
+                (TeX-open-quote "''")))
+ (bibtex-mode . ((bibtex-dialect . biblatex)
+                 (bibtex-text-indentation . 17)
+                 (bibtex-contline-indentation . 18)
+                 (fill-prefix . "                  "))))
